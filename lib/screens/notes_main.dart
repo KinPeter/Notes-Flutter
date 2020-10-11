599cc71dc5377b5450325e965470bd9a580b422c
Peter Kin <kinpeter85@gmail.com>
2020-10-11 18:58:29 +0200

    Add FAB to main screen

--- lib/screens/notes_main.dart
+++ lib/screens/notes_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes/providers/auth.dart';
 import 'package:notes/providers/notes.dart';
 import 'package:notes/screens/edit_note.dart';
 import 'package:notes/widgets/app_drawer.dart';
@@ -43,19 +44,31 @@ class _NotesMainScreenState extends State<NotesMainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Theme.of(context).backgroundColor,
-        appBar: AppBar(
-          title: Text('Notes'),
-          actions: [
-            IconButton(icon: Icon(Icons.add), onPressed: () {
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: AppBar(
+        title: Text('Notes'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditNoteScreen.routeName);
+              }),
+        ],
+      ),
+      body:
+          _isLoading ? Center(child: CircularProgressIndicator()) : NotesList(),
+      drawer: AppDrawer(),
+      floatingActionButton: Consumer<Auth>(
+        builder: (ctx, auth, child) {
+          if (!auth.isAuth || _isLoading) return Container(); // nothing
+          return FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
               Navigator.of(context).pushNamed(EditNoteScreen.routeName);
-            }),
-          ],
-        ),
-        body: _isLoading
-            ? Center(child: CircularProgressIndicator())
-            : NotesList(),
-        drawer: AppDrawer(),
+            },
+          );
+        },
+      ),
     );
   }
 }
